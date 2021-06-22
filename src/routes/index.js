6e0a69003eaded5f4dcae4976fadeb720a268e2c
LegrandMichael <michael.c.legrand@gmail.com>
2021-06-22 23:18:44 +0200

    [misc][eslint] Lancement du script pretty

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,8 @@
 import express from 'express';
-import { testEnvironmentVariable  } from '../settings';
+import { testEnvironmentVariable } from '../settings';
 
 const indexRouter = express.Router();
 
-indexRouter.get('/', (req, res) => res.status(200).json({ message: testEnvironmentVariable  }));
+indexRouter.get('/', (req, res) => res.status(200).json({ message: testEnvironmentVariable }));
 
 export default indexRouter;
