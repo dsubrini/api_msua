f8d8d1b8fc1a652ae4fc1a96101c09a8b76dca41
LegrandMichael <michael.c.legrand@gmail.com>
2021-06-22 20:41:25 +0200

    [dotEnv] chore: Lecture des variables d'environement

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,8 @@
 import express from 'express';
+import { testEnvironmentVariable  } from '../settings';
 
 const indexRouter = express.Router();
 
-indexRouter.get('/', (req, res) => res.status(200).json({ message: 'Bienvenue sur l\'api du système de gestion des utilisateurs et des annonces' }));
+indexRouter.get('/', (req, res) => res.status(200).json({ message: testEnvironmentVariable  }));
 
 export default indexRouter;
