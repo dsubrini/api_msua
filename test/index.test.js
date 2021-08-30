a6057ea3f1fa34184225fdbe9d4bdfeddddbb138
LegrandMichael <michael.c.legrand@gmail.com>
2021-08-30 11:03:11 +0200

    [misc] Changement de variable

--- test/index.test.js
+++ test/index.test.js
@@ -7,7 +7,7 @@ describe('Index de la page de test', () => {
             .expect(200)
             .end((err, res) => {
                 expect(res.status).to.equal(200);
-                expect(res.body.message).to.equal('variable bientot disponible');
+                expect(res.body.message).to.equal('hello world');
                 done();
             });
     });
