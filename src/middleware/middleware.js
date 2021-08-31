691b4e480cb48e47108523629ee748aa84cb7ef7
LegrandMichael <michael.c.legrand@gmail.com>
2021-08-31 13:55:18 +0200

    [misc] chore: Correction url de test, oubli de l'id

--- src/middleware/middleware.js
+++ src/middleware/middleware.js
@@ -7,7 +7,7 @@ export const modifyMessage = (req, res, next) => {
 
 export const performAsyncAction = async (req, res, next) => {
     try {
-        await axios.get('https://picsum.photos/id/info');
+        await axios.get('https://picsum.photos/id/0/info');
         next();
     } catch (err) {
         next(err);
