95c95089cf351a0b2c5592a5a13a43847b3281cb
LegrandMichael <michael.c.legrand@gmail.com>
2021-09-02 15:30:06 +0200

    [misc]chore: Changement de nom des variables d'environements

--- src/settings.js
+++ src/settings.js
@@ -3,8 +3,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const testEnvironmentVariable = process.env.TEST_ENV_VARIABLE;
-export const pghost = process.env.PGHOST;
-export const pguser = process.env.PGUSER;
-export const pgdb = process.env.PGDATABASE;
-export const pgpswd = process.env.PGPASSWORD;
-export const pgport = process.env.PGPORT;
+export const pghost = process.env.PG_HOST;
+export const pguser = process.env.PG_USER;
+export const pgdb = process.env.PG_DATABASE;
+export const pgpswd = process.env.PG_PASSWORD;
+export const pgport = process.env.PG_PORT;
