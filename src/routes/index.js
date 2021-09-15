d555a011950a8ba6ce540e59fbeda65610a83003
LegrandMichael <michael.c.legrand@gmail.com>
2021-09-15 12:22:33 +0200

    [feat] ajout des routes pour la consultation et la création des utilisateurs

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,17 @@
 import express from 'express';
-import { indexPage, messagesPage, addMessage } from '../controllers';
-import { modifyMessage, performAsyncAction } from '../middleware';
+import {
+    indexPage,
+    messagesPage, addMessage,
+    usersPage, addUser
+} from '../controllers';
+import { modifyMessage, modifyUser, performAsyncAction } from '../middleware';
 
 const indexRouter = express.Router();
 
 indexRouter.get('/', indexPage);
 indexRouter.get('/messages', messagesPage);
+indexRouter.get('/users', usersPage);
 indexRouter.post('/messages', modifyMessage, performAsyncAction, addMessage);
+indexRouter.post('/user_create', modifyUser, performAsyncAction, addUser);
 
 export default indexRouter;
