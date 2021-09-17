171a26d7af26d2c54f50770ee03cb7a182445d8c
LegrandMichael <michael.c.legrand@gmail.com>
2021-09-17 09:51:01 +0200

    [misc][mocha]chore: Suppression des fonctions à flèche

--- test/index.test.js
+++ test/index.test.js
@@ -1,13 +1,14 @@
 import { expect, server, BASE_URL } from './setup';
 
-describe('Index de la page de test', () => {
-    it('Afficher la page d\'accueil du site', () => {
+describe('Index de la page de test', function () {
+    it('Afficher la page d\'accueil du site', function (done) {
         server
             .get(`${BASE_URL}`)
             .expect(200)
             .end((err, res) => {
                 expect(res.status).to.equal(200);
                 expect(res.body.message).to.equal('hello world');
+                done();
             });
     });
 });
--- test/messages.test.js
+++ test/messages.test.js
@@ -1,7 +1,7 @@
 import { expect, BASE_URL, server } from './setup';
 
-describe('Messages', () => {
-    it('Afficher la page des messages', () => {
+describe('Messages', function () {
+    it('Afficher la page des messages', function (done) {
         server
             .get(`${BASE_URL}/messages`)
             .expect(200)
@@ -12,10 +12,11 @@ describe('Messages', () => {
                     expect(m).to.have.property('name');
                     expect(m).to.have.property('message');
                 });
+                done();
             });
     });
 
-    it('Ajouter un message', () => {
+    it('Ajouter un message', function (done) {
         const data = { name: 'some name', message: 'new message' };
         server
             .post(`${BASE_URL}/messages`)
@@ -29,6 +30,7 @@ describe('Messages', () => {
                     expect(m).to.have.property('name', data.name);
                     expect(m).to.have.property('message', `MODIFICATION : ${data.message}`);
                 });
+                done();
             });
     });
 });
